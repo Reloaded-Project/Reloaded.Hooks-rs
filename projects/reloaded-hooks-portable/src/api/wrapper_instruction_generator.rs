d283ee89d656e5ddd2da0e07f16206009b1f820e
sewer56lol <sewer56lol@googlemail.com>
2023-08-12 20:09:23 +0100

    WIP: Some more work on wrapper generation.

--- projects/reloaded-hooks-portable/src/api/wrapper_instruction_generator.rs
+++ projects/reloaded-hooks-portable/src/api/wrapper_instruction_generator.rs
@@ -1,4 +1,6 @@
 extern crate alloc;
+use core::hash::Hash;
+
 use alloc::vec::Vec;
 
 use super::{
@@ -10,7 +12,10 @@ use super::{
     },
     traits::register_info::RegisterInfo,
 };
-use crate::api::jit::operation_aliases::*;
+use crate::{
+    api::jit::operation_aliases::*,
+    optimize::eliminate_common_callee_saved_registers::eliminate_common_callee_saved_registers,
+};
 
 /// Options and additional context necessary for the wrapper generator.
 #[derive(Clone, Copy)]
@@ -21,6 +26,11 @@ where
     /// True if the code is within relative jump distance; and JIT
     /// can emit relative jump to target function.
     pub can_generate_relative_jumps: bool,
+
+    /// Stack offset upon entry into the wrapper generator.
+    /// This is 0 for architectures with a link register, or [usize] for architectures which have
+    /// return addresses on stack.
+    pub stack_entry_alignment: usize,
 
     /// Returns the functionalities supported by this JIT.
     /// These functionalities affect code generation performed by this library.
@@ -51,7 +61,7 @@ where
 /// - `options` - The parameters for this wrapper generation task.
 #[allow(warnings)]
 pub fn generate_wrapper_instructions<
-    TRegister: RegisterInfo + Clone,
+    TRegister: RegisterInfo + Clone + Hash + Eq,
     TFunctionAttribute: FunctionAttribute<TRegister>,
     TJit: Jit<TRegister>,
     TFunctionInfo: FunctionInfo,
@@ -65,10 +75,25 @@ pub fn generate_wrapper_instructions<
     let mut stack_pointer = 0;
 
     // Backup Always Saved Registers (LR)
-    for register in from_convention.always_saved_registers() {
+    for register in to_convention.always_saved_registers() {
         ops.push(Push::new(register.clone()).into());
         stack_pointer += register.size_in_bytes();
     }
+
+    // Backup callee saved registers
+    let callee_saved_regs = eliminate_common_callee_saved_registers(
+        from_convention.callee_saved_registers(),
+        to_convention.callee_saved_registers(),
+    );
+
+    for register in callee_saved_regs {
+        ops.push(Push::new(register.clone()).into());
+        stack_pointer += register.size_in_bytes();
+    }
+
+    // Reserve required space
+    // let sp = RegisterInfo::stack_pointer();
+    // ops.push(Sub::new(sp, from_convention.reserved_stack_space() as i32).into());
 
     // Return Result
     todo!("rest of code");
