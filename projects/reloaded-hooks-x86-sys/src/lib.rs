60d7ee1e178ee564c9d9189f9170ec83f02ce6cd
sewer56 <sewer56lol@googlemail.com>
2023-09-05 16:22:29 +0100

    [x86] Ignore Coverage on JIT Conversions

--- projects/reloaded-hooks-x86-sys/src/lib.rs
+++ projects/reloaded-hooks-x86-sys/src/lib.rs
@@ -2,10 +2,12 @@
 //! Here's the crate documentation.
 #![cfg_attr(not(test), no_std)]
 
+#[cfg(not(tarpaulin_include))]
 pub mod all_registers;
 pub mod jit_common;
 
 #[allow(dead_code)]
+#[cfg(not(tarpaulin_include))]
 pub mod jit_conversions_common;
 
 pub mod preset_calling_convention;
