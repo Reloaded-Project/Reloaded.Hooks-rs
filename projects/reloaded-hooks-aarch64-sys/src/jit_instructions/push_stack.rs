b556476201cd6ee8fd9e3ab5676410c2c6f0d45c
sewer56 <sewer56lol@googlemail.com>
2023-09-30 16:19:39 +0100

    Changed: Disable push_stack for now

--- projects/reloaded-hooks-aarch64-sys/src/jit_instructions/push_stack.rs
+++ projects/reloaded-hooks-aarch64-sys/src/jit_instructions/push_stack.rs
@@ -19,6 +19,8 @@ pub fn encode_push_stack(
     }
 
     let num_regs = x.num_scratch_registers();
+    let mut remaining_bytes = x.item_size;
+
     if num_regs >= 2 {
         todo!();
     } else if num_regs == 1 {
@@ -29,8 +31,7 @@ pub fn encode_push_stack(
         ));
     }
 
-    let mut remaining_bytes = x.item_size;
-
+    /*
     while remaining_bytes > 0 {
         if remaining_bytes >= 16 {
             // Push Multiple Register
@@ -43,6 +44,7 @@ pub fn encode_push_stack(
             remaining_bytes -= 4;
         }
     }
+    */
 
     Ok(())
 }
@@ -50,37 +52,40 @@ pub fn encode_push_stack(
 #[cfg(test)]
 mod tests {
 
-    use crate::jit_instructions::stackalloc::encode_stackalloc;
+    use crate::all_registers::AllRegisters;
+    use crate::all_registers::AllRegisters::*;
+    use crate::jit_instructions::push_stack::encode_push_stack;
     use crate::test_helpers::instruction_buffer_as_hex;
     use reloaded_hooks_portable::api::jit::operation_aliases::*;
     use rstest::rstest;
 
+    /*
     #[rstest]
-    #[case(4, "ff1300d1", false)]
-    #[case(-4, "ff130091", false)]
-    #[case(0, "ff0300d1", false)]
-    #[case(2048, "ff0320d1", false)]
-    #[case(-2048, "ff032091", false)]
-    fn test_encode_stackalloc(
-        #[case] operand: i32,
+    #[case(4, 4, Some(x10), Some(x11), "ff1300d1", false)]
+    fn test_encode_pushstack(
+        #[case] offset: i32,
+        #[case] item_size: u32,
+        #[case] scratch_1: Option<AllRegisters>,
+        #[case] scratch_2: Option<AllRegisters>,
         #[case] expected_hex: &str,
         #[case] is_err: bool,
     ) {
         let mut pc = 0;
         let mut buf = Vec::new();
-        let operation = StackAlloc { operand };
+        let operation = PushStack::new(offset, item_size, scratch_1, scratch_2);
 
         // Check for errors if applicable
         if is_err {
-            assert!(encode_stackalloc(&operation, &mut pc, &mut buf).is_err());
+            assert!(encode_push_stack(&operation, &mut pc, &mut buf).is_err());
             return;
         }
 
         // If the encoding is successful, compare with the expected hex value
-        assert!(encode_stackalloc(&operation, &mut pc, &mut buf).is_ok());
+        assert!(encode_push_stack(&operation, &mut pc, &mut buf).is_ok());
         assert_eq!(expected_hex, instruction_buffer_as_hex(&buf));
 
         // Assert that the program counter has been incremented by 4
         assert_eq!(4, pc);
     }
+    */
 }
