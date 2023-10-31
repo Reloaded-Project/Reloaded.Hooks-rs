02a40445e78e82a2eb6b8273d87960d0b058004d
sewer56 <sewer56lol@googlemail.com>
2023-10-31 21:49:32 +0000

    Added: Support for rewriting bl

--- projects/reloaded-hooks-aarch64-sys/src/code_rewriter/b.rs
+++ projects/reloaded-hooks-aarch64-sys/src/code_rewriter/b.rs
@@ -33,6 +33,7 @@ pub(crate) fn rewrite_b(
     old_address: usize,
     new_address: usize,
     scratch_register: Option<u8>,
+    link: bool,
 ) -> Result<InstructionRewriteResult, CodeRewriterError> {
     let orig_ins = B(instruction.to_le());
     let orig_target = (old_address as isize).wrapping_add(orig_ins.offset() as isize);
@@ -46,16 +47,16 @@ pub(crate) fn rewrite_b(
         // Otherwise if further away from 4G, use MOVZ + MOVK + BR.
         if !(-0x40000000..=0x3FFFFFFF).contains(&new_offset) {
             // MOV <immediate> + BR
-            return rewrite_b_immediate(orig_target as usize, scratch_reg);
+            return rewrite_b_immediate(orig_target as usize, scratch_reg, link);
         }
 
         // ADRP + (ADD) + BR
-        return rewrite_b_4gib(new_address, orig_target as usize, scratch_reg);
+        return rewrite_b_4gib(new_address, orig_target as usize, scratch_reg, link);
     }
 
     // Rewrite as regular branch.
     // - B
-    let instruction = B::assemble_b(new_offset as i32);
+    let instruction = B::assemble(new_offset as i32, link);
     Ok(InstructionRewriteResult::B(instruction.unwrap().0.to_le()))
 }
 
@@ -63,6 +64,7 @@ pub(crate) fn rewrite_b_4gib(
     new_address: usize,
     target_address: usize,
     scratch_register: u8,
+    link: bool,
 ) -> Result<InstructionRewriteResult, CodeRewriterError> {
     // Assemble ADRP + ADD if out of range.
     // This will error if our address is too far.
@@ -74,8 +76,7 @@ pub(crate) fn rewrite_b_4gib(
         .unwrap()
         .0;
 
-    let branch = BranchRegister::new_br(scratch_register).0;
-
+    let branch = BranchRegister::new_b(scratch_register, link).0;
     let remainder = target_address - adrp_target_address;
     if remainder > 0 {
         let add = AddImmediate::new(true, scratch_register, scratch_register, remainder as u16)
@@ -93,9 +94,10 @@ pub(crate) fn rewrite_b_4gib(
 pub(crate) fn rewrite_b_immediate(
     orig_target: usize,
     scratch_register: u8,
+    link: bool,
 ) -> Result<InstructionRewriteResult, CodeRewriterError> {
-    let mov_instr = emit_mov_const_to_reg(scratch_register, orig_target as usize); // Assuming emit_mov_const_to_reg can handle this as well
-    let b_instr = BranchRegister::new_br(scratch_register);
+    let mov_instr = emit_mov_const_to_reg(scratch_register, orig_target); // Assuming emit_mov_const_to_reg can handle this as well
+    let b_instr = BranchRegister::new_b(scratch_register, link);
 
     let mut result = Vec::new();
     mov_instr.append_to_buffer(&mut result);
@@ -119,7 +121,7 @@ mod tests {
     #[case::adrp_and_br(0x00040014_u32.to_be(), 0x8000000, 0, "110004b020021fd6")]
     // [Within 4GiB] || b #4096 -> adrp + add + br || (We add +128MiB + 512 to branch offset)
     #[case::adrp_and_add_and_br(0x00040014_u32.to_be(), 0x8000512, 0, "110004b0314a149120021fd6")]
-    // [Last Resort, Move] || b.eq #0 -> mov immediate + br x17 || (We add +4GiB to branch offset)
+    // [Last Resort, Move] || b #4096 -> mov immediate + br x17 || (We add +4GiB to branch offset)
     #[case::mov_and_br(0x00040014_u32.to_be(), 0x100000000, 0, "110082d21100a0f23100c0f220021fd6")]
     fn test_rewrite_b(
         #[case] old_instruction: u32,
@@ -127,7 +129,26 @@ mod tests {
         #[case] new_address: usize,
         #[case] expected_hex: &str,
     ) {
-        let result = rewrite_b(old_instruction, old_address, new_address, Some(17)).unwrap();
+        let result = rewrite_b(old_instruction, old_address, new_address, Some(17), false).unwrap();
+        assert_eq!(result.to_hex_string(), expected_hex);
+    }
+
+    #[rstest]
+    // [Within 128MiB] || b #4096 -> b #8192 || (We add 4096 to branch offset.)
+    #[case::simple_b(0x00040094_u32.to_be(), 8192, 4096, "00080094")]
+    // [Within 4GiB] || b #4096 -> adrp + br || (We add +128MiB to branch offset)
+    #[case::adrp_and_br(0x00040094_u32.to_be(), 0x8000000, 0, "110004b020023fd6")]
+    // [Within 4GiB] || b #4096 -> adrp + add + br || (We add +128MiB + 512 to branch offset)
+    #[case::adrp_and_add_and_br(0x00040094_u32.to_be(), 0x8000512, 0, "110004b0314a149120023fd6")]
+    // [Last Resort, Move] || b #4096 -> mov immediate + br x17 || (We add +4GiB to branch offset)
+    #[case::mov_and_br(0x00040094_u32.to_be(), 0x100000000, 0, "110082d21100a0f23100c0f220023fd6")]
+    fn test_rewrite_bl(
+        #[case] old_instruction: u32,
+        #[case] old_address: usize,
+        #[case] new_address: usize,
+        #[case] expected_hex: &str,
+    ) {
+        let result = rewrite_b(old_instruction, old_address, new_address, Some(17), true).unwrap();
         assert_eq!(result.to_hex_string(), expected_hex);
     }
 }
--- projects/reloaded-hooks-aarch64-sys/src/instructions/b.rs
+++ projects/reloaded-hooks-aarch64-sys/src/instructions/b.rs
@@ -20,22 +20,49 @@ bitfield! {
 }
 
 impl B {
-    /// Assembles a B instruction with the specified offset.
-    pub fn assemble_b(offset: i32) -> Result<Self, JitError<AllRegisters>> {
+    fn assemble_common(
+        instruction_name: &str,
+        offset: i32,
+        opcode: u8,
+    ) -> Result<Self, JitError<AllRegisters>> {
         #[cfg(debug_assertions)]
         if !(-0x8000000..=0x7FFFFFF).contains(&offset) {
-            return Err(exceeds_maximum_range("[B]", "-+128MiB", offset as isize));
+            return Err(exceeds_maximum_range(
+                instruction_name,
+                "-+128MiB",
+                offset as isize,
+            ));
         }
+
         #[cfg(debug_assertions)]
         if (offset & 0b11) != 0 {
-            return Err(must_be_divisible_by("[B]", offset as isize, 4));
+            return Err(must_be_divisible_by(instruction_name, offset as isize, 4));
         }
 
         let mut instruction = B(0);
-        instruction.set_opcode(0b000101);
+        instruction.set_opcode(opcode);
         instruction.set_imm26(offset / 4);
 
         Ok(instruction)
+    }
+
+    /// Assembles a B or BL instruction with the specified offset.
+    pub fn assemble(offset: i32, link: bool) -> Result<Self, JitError<AllRegisters>> {
+        if link {
+            Self::assemble_bl(offset)
+        } else {
+            Self::assemble_b(offset)
+        }
+    }
+
+    /// Assembles a B instruction with the specified offset.
+    pub fn assemble_b(offset: i32) -> Result<Self, JitError<AllRegisters>> {
+        Self::assemble_common("[B]", offset, 0b000101)
+    }
+
+    /// Assembles a BL instruction with the specified offset.
+    pub fn assemble_bl(offset: i32) -> Result<Self, JitError<AllRegisters>> {
+        Self::assemble_common("[BL]", offset, 0b100101)
     }
 
     pub fn offset(&self) -> i32 {
