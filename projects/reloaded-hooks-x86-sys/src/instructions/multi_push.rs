c978c0eb1594a95e35f558a15b272eea73cf72d8
sewer56 <sewer56lol@googlemail.com>
2023-12-27 01:17:25 +0000

    [x64] Fixed MultiPush / MultiPop

--- projects/reloaded-hooks-x86-sys/src/instructions/multi_push.rs
+++ projects/reloaded-hooks-x86-sys/src/instructions/multi_push.rs
@@ -41,8 +41,17 @@ pub(crate) fn encode_multi_push(
     }
 
     // Reserve the space.
-    a.sub(iced_regs::esp, space_needed as i32)
-        .map_err(convert_error)?;
+    if a.bitness() == 64 && cfg!(feature = "x64") {
+        a.sub(iced_regs::rsp, space_needed as i32)
+            .map_err(convert_error)?;
+    } else if a.bitness() == 32 && cfg!(feature = "x86") {
+        a.sub(iced_regs::esp, space_needed as i32)
+            .map_err(convert_error)?;
+    } else {
+        return Err(JitError::ThirdPartyAssemblerError(
+            ARCH_NOT_SUPPORTED.to_string(),
+        ));
+    }
 
     // Push the items.
     let mut current_offset = 0;
@@ -86,32 +95,31 @@ mod tests {
     Push::new(x64::Register::rax),
     Push::new(x64::Register::rbx),
     Push::new(x64::Register::rcx),
-])], "83ec1848890c2448895c24084889442410")]
+    ])], "83ec1848890c2448895c24084889442410")]
     // XMM register push for x64
     #[case::compile_multi_push_xmm_x64(vec![Op::MultiPush(smallvec![
     Push::new(x64::Register::xmm0),
     Push::new(x64::Register::xmm1),
     Push::new(x64::Register::xmm2),
-])], "83ec30f30f7f1424f30f7f4c2410f30f7f442420")]
+    ])], "83ec30f30f7f1424f30f7f4c2410f30f7f442420")]
     // YMM register push for x64
     #[case::compile_multi_push_ymm_x64(vec![Op::MultiPush(smallvec![
     Push::new(x64::Register::ymm0),
     Push::new(x64::Register::ymm1),
     Push::new(x64::Register::ymm2),
-])], "83ec60c5fe7f1424c5fe7f4c2420c5fe7f442440")]
+    ])], "83ec60c5fe7f1424c5fe7f4c2420c5fe7f442440")]
     // ZMM register push for x64
     #[case::compile_multi_push_zmm_x64(vec![Op::MultiPush(smallvec![
     Push::new(x64::Register::zmm0),
     Push::new(x64::Register::zmm1),
     Push::new(x64::Register::zmm2),
-])], "81ecc000000062f17f487f142462f17f487f4c240162f17f487f442402")]
+    ])], "81ecc000000062f17f487f142462f17f487f4c240162f17f487f442402")]
     // Mixed register push for x64
     #[case::compile_multi_push_mixed_x64(vec![Op::MultiPush(smallvec![
     Push::new(x64::Register::rax),
     Push::new(x64::Register::xmm0),
     Push::new(x64::Register::ymm1),
-])], "83ec38c5fe7f0c24f30f7f4424204889442430")]
-
+    ])], "83ec38c5fe7f0c24f30f7f4424204889442430")]
     fn multi_push_x64(#[case] operations: Vec<Op<x64::Register>>, #[case] expected_hex: &str) {
         let result = JitX64::compile(0, &operations);
         assert!(result.is_ok());
@@ -124,32 +132,31 @@ mod tests {
     Push::new(x86::Register::eax),
     Push::new(x86::Register::ebx),
     Push::new(x86::Register::ecx),
-])], "83ec0c890c24895c240489442408")]
+    ])], "83ec0c890c24895c240489442408")]
     // XMM register push for x86
     #[case::compile_multi_push_xmm_x86(vec![Op::MultiPush(smallvec![
     Push::new(x86::Register::xmm0),
     Push::new(x86::Register::xmm1),
     Push::new(x86::Register::xmm2),
-])], "83ec30f30f7f1424f30f7f4c2410f30f7f442420")]
+    ])], "83ec30f30f7f1424f30f7f4c2410f30f7f442420")]
     // YMM register push for x86
     #[case::compile_multi_push_ymm_x86(vec![Op::MultiPush(smallvec![
     Push::new(x86::Register::ymm0),
     Push::new(x86::Register::ymm1),
     Push::new(x86::Register::ymm2),
-])], "83ec60c5fe7f1424c5fe7f4c2420c5fe7f442440")]
+    ])], "83ec60c5fe7f1424c5fe7f4c2420c5fe7f442440")]
     // ZMM register push for x86
     #[case::compile_multi_push_zmm_x86(vec![Op::MultiPush(smallvec![
     Push::new(x86::Register::zmm0),
     Push::new(x86::Register::zmm1),
     Push::new(x86::Register::zmm2),
-])], "81ecc000000062f17f487f142462f17f487f4c240162f17f487f442402")]
+    ])], "81ecc000000062f17f487f142462f17f487f4c240162f17f487f442402")]
     // Mixed register push for x86
     #[case::compile_multi_push_mixed_x86(vec![Op::MultiPush(smallvec![
     Push::new(x86::Register::eax),
     Push::new(x86::Register::xmm0),
     Push::new(x86::Register::ymm1),
-])], "83ec34c5fe7f0c24f30f7f44242089442430")]
-
+    ])], "83ec34c5fe7f0c24f30f7f44242089442430")]
     fn multi_push_x86(#[case] operations: Vec<Op<x86::Register>>, #[case] expected_hex: &str) {
         let result = JitX86::compile(0, &operations);
         assert!(result.is_ok());
